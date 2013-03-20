503609df0c3d7ed98c8d3c0dcadaa9fcb3ba5444
finkultur <viktor.w.nilsson@gmail.com>
2013-03-20 14:54:12 +0100

    redirect output to file

--- main.c
+++ main.c
@@ -9,28 +9,32 @@
 
 #include "parser.c"
 
-int startAllNow(struct cmdEntry *firstEntry);
+int startAllNow(struct cmdEntry *firstEntry, int numOfProcs);
 //int parseFile(const char *filepath, struct cmdEntry *firstEntry);
 //int parseLine(const char *line, struct cmdEntry *entry);
 
 /**
  * Main function.
  *
- * usage: ./main <workloadfile>
+ * usage: ./main <workloadfile> <logfile>
  */
 int main(int argc, char *argv[]) {
 
     // Check command line arguments
-    if (argc != 2) {
-        printf("usage: %s <inputfile>\n", argv[0]);
+    if (argc != 3) {
+        printf("usage: %s <inputfile> <logfile>\n", argv[0]);
         return 1; // Error!
     }
-    printf("DFS scheduler initalized.\n");
+    // Some checks should be made here
+    char *logfile = argv[2];
+
+    printf("DFS scheduler initalized, writing output to %s\n", logfile);
+    FILE *fp;
+    fp = freopen(logfile, "a+", stdout);
 
     struct cmdEntry *entry = malloc(sizeof(struct cmdEntry));
-    /*int numOfEntries = */
-    parseFile(argv[1], entry);
-    startAllNow(entry);
+    int numOfEntries = parseFile(argv[1], entry);
+    startAllNow(entry, numOfEntries);
 
     return 0;
 }
@@ -38,28 +42,36 @@ int main(int argc, char *argv[]) {
 /* Starts all processes.
  * Takes the first entry and proceedes until nextentry is NULL
  */
-int startAllNow(struct cmdEntry *entryProcess) {
+int startAllNow(struct cmdEntry *entryProcess, int numOfProcs) {
     
     struct cmdEntry *processToRun = entryProcess;
     int pid;
 
-    for(int i=0; processToRun != NULL; i++) {
+    for (int i=0; processToRun != NULL; i++) {
 
         pid = fork();
         if (pid == 0) { // Child process
             //printf("starting command #%u: %s,(sent from child)\n", i, processToRun->cmd); 
             sleep(processToRun->start_time);
             chdir(processToRun->cwd);
+            
+            // SCHEDULE HERE?
             //tmc_set_my_affinity(i); // tile #i kansle är upptagen?
+
             int status = execvp(processToRun->cmd, (char **)processToRun->args);
-            printf("execvp exit with status: %d\n", status);
+            printf("execvp exited with status: %d\n", status);
             return 1;
         }
         else { // Parent process
-            //wait(NULL);
+            wait(NULL);
         }
         processToRun = processToRun->nextEntry;
 
     }
+
+    //for (int i=0; i<numOfProcs; i++) {
+    //    wait(NULL);
+    //}   
+ 
     return 0;
 }
