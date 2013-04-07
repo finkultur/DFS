281d17ec5d252651b47bf0817c95ef2c29fac158
finkultur <viktor.w.nilsson@gmail.com>
2013-04-07 12:53:51 +0200

    just a script that builds and runs tile-monitor

---
+++ run.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+# run.sh 
+# Run this with ./run <workload-file>
+
+make
+/opt/tilepro/bin/tile-monitor --tile 4x2 --pci --here --mount-same /opt/benchmarks/SPEC2006/benchspec/CPU2006/ --hv-bin-dir /scratch/src/sys/hv/ -- ./main $1
