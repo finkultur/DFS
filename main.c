75fab9595c66e7a6f1bf52bc1b25f2c8990b60c4
finkultur <viktor.w.nilsson@gmail.com>
2013-04-18 11:40:46 +0200

    Removed unnecessary files

--- main.c
+++ main.c
@@ -18,30 +18,22 @@
 #include <tmc/udn.h>
 
 // DFS 
-#include "pid_table.h"
 #include "cmd_list.h"
-#include "sched_algs.h"
-#include "perfcount.h"
 
-#define NUM_OF_CPUS 16
-#define TABLE_SIZE 8
+#define NUM_OF_CPUS 8
 
 // RTS handlers:
 void start_handler(int, siginfo_t*, void*);
-void end_handler(int, siginfo_t*, void*);
 
 // Functions that probably shouldn't be defined in main
 int start_process(void);
-int children_is_still_alive(void);
-void print_tileAlloc(void);
 
 // Global values:
 int counter = 0;
-pid_table table;
-int tileAlloc[NUM_OF_CPUS];
 cmd_list list;
 cpu_set_t cpus;
 int last_program_started = 0;
+int childrens = 0;
 
 /**
  * Main function.
@@ -52,16 +44,10 @@ int main(int argc, char *argv[]) {
 
 	// RTS actions:
     struct sigaction start_action;
-    //struct sigaction end_action;
 
     // Save starting time
     long long int start_time = time(NULL);
     printf("Start time is: %lld\n", start_time);
-
-    // Initialize tileAlloc to zeros, this shouldn't be necessary (?)
-    for (int i=0;i<NUM_OF_CPUS;i++) {
-        tileAlloc[i] = 0;
-    }
 
     // Check command line arguments
     if (argc < 2 || argc > 3) {
@@ -86,15 +72,6 @@ int main(int argc, char *argv[]) {
         tmc_task_die("Insufficient cpus available.");
     }
 
-    // Setup all performance counters on every initialized tile
-    if (setup_all_counters(&cpus) != 0) {
-        printf("setup_all_counters failed\n");
-        return 1;
-    }
-
-    // Initialize pid_table
-    table = create_pid_table(TABLE_SIZE);
-
     // Parse the file and set next to first entry in file.
     if ((list = create_cmd_list(argv[1])) == NULL) {
         printf("Failed to create command list from file: %s\n", argv[1]);
@@ -113,38 +90,19 @@ int main(int argc, char *argv[]) {
     	printf("Failed to setup handler for SIGALRM\n");
     }
     
-    /*
-    // Init RTS for process termination:
-    end_action.sa_handler = NULL;
-    end_action.sa_flags = SA_SIGINFO;
-    end_action.sa_sigaction = end_handler;
-    // Install handler (blocking SIGCHLD/SIGALRM during handler execution):
-    if (sigemptyset(&end_action.sa_mask) != 0
-    	|| sigaddset(&end_action.sa_mask, SIGCHLD) != 0
-    	|| sigaddset(&end_action.sa_mask, SIGALRM) != 0
-    	|| sigaction(SIGCHLD, &end_action, NULL) != 0) {
-    	printf("Failed to setup handler for SIGCHLD\n");
-    }
-    */
-    
     // Start the first process(es) in file and setup timers and stuff.
     start_process();
     
     // While the last process hasn't started and children is still alive,
     // reap the dying children.
     int child_pid;
-    int child_tile_num;
-    while(children_is_still_alive() || last_program_started == 0) {
+    while(childrens > 0 || last_program_started == 0) {
         child_pid = wait(NULL);
         if (child_pid > 0) {
-            child_tile_num = get_tile_num(table, child_pid);
-            tileAlloc[child_tile_num]--;
-            remove_pid(table, child_pid);
+            childrens--;
+            printf("Number of unfinished children: %i", childrens);
         }
     }
-
-    // This function (usually) prints performance counters & their miss rate
-    get_tile_with_min_write_miss_rate(&cpus);
 
     // Print start, end and total time elapsed.
     long long int end_time = time(NULL);
@@ -167,55 +125,29 @@ void start_handler(int signo, siginfo_t *info, void *context) {
 }
 
 /*
- * Handles SIGCHLD interrupts sent by dying children.
- */
-/*void end_handler(int signo, siginfo_t *info, void *context)
-{
-    int pid;
-    pid = wait(NULL);
-
-    // Get what tile the pid ran on
-    int tile_num = get_tile_num(table, pid);
-    // Decrement the number of processes on that tile
-    tileAlloc[tile_num]--;
-    // Remove pid from pid_table
-    remove_pid(table, pid);
-    // Printing from signal handlers is stupid:
-    //printf("pid: %d is done.\n", pid);
-}*/
-
-/*
  * Starts all processes with start_time less than or equal to the current
  * time/counter. Derps with the pid table, allocates a specific tile
  * to the process(es) and forks child process(es). Also keeps track of the
  * next set of program to start.
  */
 int start_process() {
-
     struct itimerval timer;
     struct timeval timeout;
     cmd_entry cmd; 
  
     while ((cmd = get_first(list)) != NULL && cmd->start_time <= counter) {
-        // Try to get an empty tile (or the tile with least contention)
-        int tile_num = get_tile(&cpus, tileAlloc);
-        // Increment the number of processes running on that tile.
-        tileAlloc[tile_num]++;
-
+        printf("Time %i: Start command: %s (not showing args)\n", 
+               cmd->start_time, cmd->cmd);
         int pid = fork();
         if (pid < 0) {
             return 1; // Fork failed
         }
         else if (pid == 0) { // Child process
-            if (tmc_cpus_set_my_cpu(tmc_cpus_find_nth_cpu(&cpus, tile_num)) < 0) {
-                tmc_task_die("failure in 'tmc_set_my_cpu'");
-            }
-            
             /*   
             int mypid = getpid();
             int mycurcpu = tmc_cpus_get_my_current_cpu();
-            printf("I am pid #%i and I am on physical tile #%i, logical tile #%i\n", 
-                   mypid, mycurcpu, tile_num);
+            printf("I am pid #%i and I am on physical tile #%i\n", 
+                   mypid, mycurcpu);
             */
             chdir(cmd->dir);
             int status = execv(cmd->cmd, (char **)cmd->argv);
@@ -223,10 +155,7 @@ int start_process() {
             return 1;
         }
         else {
-            // Add pid to table
-            add_pid(table, pid, tile_num);
-            // Print table for debugging purposes
-            //print_table(table);
+            childrens++;
         }
         remove_first(list);
     }
@@ -242,39 +171,6 @@ int start_process() {
     else {
         last_program_started = 1;
     }
-    // Disable timer. I got some weird segmentation fault with this:
-    /*else { 
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 0;
-        timer.it_interval = timeout;
-        timer.it_value = timeout;
-        counter = cmd->start_time;
-        setitimer(ITIMER_REAL, &timer, NULL);
-    }*/
     return 0;
 }
 
-/*
- * Prints the tileAlloc array
- */
-void print_tileAlloc() {
-    printf("Process allocation: ");
-    for (int i=0;i<NUM_OF_CPUS;i++) {
-        printf("%i:%i, ", i, tileAlloc[i]);
-    }
-    printf("\n");
-}
-
-/*
- * Checks if there are running child processes.
- * Returns 1 if any children is still alive.
- * Isn't it a syscall for this? 
- */
-int children_is_still_alive() {
-    for (int i=0;i<NUM_OF_CPUS;i++) {
-        if (tileAlloc[i] != 0) {
-            return 1;
-        }
-    }
-    return 0;
-} 
