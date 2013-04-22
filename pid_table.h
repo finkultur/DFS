5bc5bf49c28b81eddab427b6bc7fc5df267c0a9c
finkultur <viktor.w.nilsson@gmail.com>
2013-04-22 22:50:17 +0200

    Added files from pid_table

--- pid_table.h
+++ pid_table.h
@@ -1,61 +1,53 @@
 /* pid_table.h
  *
- * A simple hash-table like module that is used to store data about process-id
- * to tile (cpu core) allocation.
+ * A hash table module used to record process-to-tile (CPU core) allocation.
+ * The table is indexed by a simple value calculated from the process ID modulo
+ * the size of the hash index.
+ * To improve the runtime execution performance, the data buckets on each index
+ * are implemented with a dynamically sized vector. When creating a table, it
+ * is therefore important to consider the initial size of both the hash index
+ * and the data bucket vectors. These values should be properly adjusted to
+ * ensure that the table will fit the expected number of elements in an
+ * effective manner.
+ * To further improve performance, especially targeting lookup speed, the data
+ * buckets are kept sorted (by process ID) to allow binary searching.
  */
 
 #ifndef _PID_TABLE_H
 #define _PID_TABLE_H
 
 #include <unistd.h>
-#include <sys/types.h>
 
-/* Each table instance is represented by a pid_table_struct. */
+/* Each table instance is represented by a table_struct. */
 struct pid_table_struct;
 
-/* Each table entry is represented by a pid_entry_struct. This struct holds
- * data about the process-id and the number of the tile allocated to the
- * process. */
-struct pid_entry_struct;
-
-/* Typedef for a table pointer. */
+/* Typedef for a user handle to a table instance (table pointer). */
 typedef struct pid_table_struct *pid_table;
 
-/* Typedef for a table entry pointer. */
-typedef struct pid_entry_struct *pid_entry;
-
-/* Creates a new table of the desired size and returns a pointer to it. On
- * failure a NULL-pointer is returned. */
-pid_table create_pid_table(size_t size);
+/* Allocates a new table with the specified sizes for the hash index and the
+ * data bucket vectors.
+ * On success a handle to the table is returned, otherwise NULL. */
+pid_table create_pid_table(size_t index_size, size_t bucket_count);
 
 /* Destroys the given table by freeing up allocated memory. All table entries
  * are also freed during the operation. */
 void destroy_pid_table(pid_table table);
 
-/* Get the size of the specified table. */
-size_t get_size(pid_table table);
-
-/* Get the current number of entries in the specified table. */
-int get_count(pid_table table);
-
-/* Adds a new entry to the table with the specified process-id and allocated
+/* Adds a new entry to the table with the specified process ID and allocated
  * tile. On success 0 is returned, otherwise -1. */
-int add_pid(pid_table table, pid_t pid, int tile_num);
+int add_pid(pid_table table, pid_t pid, unsigned int cpu);
 
-/* Removes the entry with the specified pid from the table. Returns 0 on
+/* Removes the entry with the specified process ID from the table. Returns 0 on
  * success, otherwise -1. */
 int remove_pid(pid_table table, pid_t pid);
 
-/* Tries to find an entry with the specified pid and return the number of the
- * tile allocated to the process. On success the tile number is returned,
+/* Tries to find an entry with the specified process ID and return the number
+ * of the tile allocated to the process. On success the tile number is returned,
  * otherwise -1. */
-int get_tile_num(pid_table table, pid_t pid);
+int get_cpu(pid_table table, pid_t pid);
 
-/* Sets the tile number for the specified process-id to the specified value.
+/* Sets the tile number for the specified process ID to the specified value.
  * On success 0 is returned, otherwise -1. */
-int set_tile_num(pid_table table, pid_t pid, int tile_num);
-
-// FOR TESTING:
-void print_table(pid_table table);
+int set_cpu(pid_table table, pid_t pid, unsigned int cpu);
 
 #endif /* _PID_TABLE_H */
