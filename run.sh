b02fe4b22afaa6292a7f746577c6cfff487a1e55
finkultur <viktor.w.nilsson@gmail.com>
2013-05-10 19:36:06 +0200

    added new runfile and workloads

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Run this with ./run <workload-file>
 
 make
-/opt/tilepro/bin/tile-monitor --tile 4x4 --pci --here --mount-same /opt/benchmarks/SPEC2006/benchspec/CPU2006/ --hv-bin-dir /scratch/src/sys/hv/ -- ./main $1
+/opt/tilepro/bin/tile-monitor --tile 8x8 --pci --here --mount-same /opt/benchmarks/SPEC2006/benchspec/CPU2006/ --hvc /scratch/vmlinux-pci.hvc --hv-bin-dir /scratch/src/sys/hv/ -- ./main $1
